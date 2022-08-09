a1c8f48dd54d7a703dc49b07ad2e1b4eb7dc1407
key1sam <seongminkim.dev@gmail.com>
2022-08-10 03:16:41 +0900

    오타 수정

--- oidc/src/components/Page/BatchInstallSecu.js
+++ oidc/src/components/Page/BatchInstallSecu.js
@@ -17,7 +17,7 @@ import $ from "jquery";
          <Card style={{ width: '80rem', height: '40rem', display: 'flex', position: 'relative', }}>
             <Card.Body style = {{position: 'absolute', top:'50%', left:'50%', transform: 'translate(-50%, -50%)'}}>
               <Card.Title style = {{textAlign : 'center', fontWeight: 'bold', fontSize : '30px', marginBottom : '15px', width : '1200px'}}>추가하려는 Server에 아래 명령어를 순서대로 입력하여</Card.Title>
-              <Card.Title style = {{textAlign : 'center', fontWeight: 'bold', fontSize : '30px', marginBottom : '15px', width : '1200px'}}>batch 파일을 서버에 다운로드/실행해주세요.</Card.Title>
+              <Card.Title style = {{textAlign : 'center', fontWeight: 'bold', fontSize : '30px', marginBottom : '15px', width : '1200px'}}>batch 파일을 서버에 다운로드 및 실행해주세요.</Card.Title>
               <div style = {{textAlign : 'center', fontWeight: 'bold', fontSize : '20px', marginBottom : '15px', width : '1200px', color: 'red'}}>(+Allow in from 'Your IP' to 'Zabbix Proxy IP' proto tcp port 10050)</div>
               <div style = {{textAlign : 'center', fontWeight: 'bold', fontSize : '20px', marginBottom : '15px', width : '1200px', color: 'red'}}>(+Allow out from 'Zabbix Proxy IP' to 'Your IP' proto tcp port 10050)</div>
               <hr></hr> 
@@ -71,7 +71,7 @@ import $ from "jquery";
               <Card style={{ width: '80rem', height: '40rem', display: 'flex', position: 'relative', }}>
             <Card.Body style = {{position: 'absolute', top:'50%', left:'50%', transform: 'translate(-50%, -50%)'}}>
               <Card.Title style = {{textAlign : 'center', fontWeight: 'bold', fontSize : '30px', marginBottom : '15px', width : '1200px'}}>추가하려는 Server에 아래 명령어를 순서대로 입력하여</Card.Title>
-              <Card.Title style = {{textAlign : 'center', fontWeight: 'bold', fontSize : '30px', marginBottom : '15px', width : '1200px'}}>batch 파일을 서버에 다운로드/실행해주세요.</Card.Title>
+              <Card.Title style = {{textAlign : 'center', fontWeight: 'bold', fontSize : '30px', marginBottom : '15px', width : '1200px'}}>batch 파일을 서버에 다운로드 및 실행해주세요.</Card.Title>
               <div style = {{textAlign : 'center', fontWeight: 'bold', fontSize : '20px', marginBottom : '15px', width : '1200px', color: 'red'}}>(+Allow in from 'Your IP' to 'Zabbix Proxy IP' proto tcp port 10050)</div>
               <div style = {{textAlign : 'center', fontWeight: 'bold', fontSize : '20px', marginBottom : '15px', width : '1200px', color: 'red'}}>(+Allow out from 'Zabbix Proxy IP' to 'Your IP' proto tcp port 10050)</div>
               <hr></hr> 
